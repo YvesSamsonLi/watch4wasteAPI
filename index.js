02f9f946924153be3594f7003e51bd2135491561
YvesSamsonLi <76028406+YvesSamsonLi@users.noreply.github.com>
2021-07-11 12:39:21 +0800

    setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000
+
+// Connecting to MongoDB Atlas
+mongoose.connect(process.env.MONGO_URL, 
+{useNewUrlParser:true}
+)
+.then(() =>{
+    console.log("Connected to MongoDB Atlas");
+}).catch(error => {
+    console.log("Something wrong happened", error);
+});
+
+//start server
+app.listen(PORT, () => {
+    console.log("Server started at PORT: ", PORT);
+})
